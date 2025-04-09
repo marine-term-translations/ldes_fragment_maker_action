698c491aacb0d736ccee5a925c4f1153143bef82
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2025-04-09 15:03:08 +0200

    Refactor entrypoint script to streamline LDES fragment handling; improve branch management and synchronization

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,31 +21,39 @@ git config --global --add safe.directory /github/workspace
 # set user name and email 
 git config --global user.name 'GitHub Actions'
 git config --global user.email 'actions@github.com'
+git fetch --all
+BRANCHES=$(git branch -a)
+echo "$BRANCHES"
 
 cd ../..
 cd src
 
 python fragment_maker.py --branch $INPUT_BRANCH
 
+cd ..
+cd ./github/workspace
+git checkout main
+cd ../..
+rsync --recursive --progress -avzhq --exclude=.git --exclude=.github --exclude=.dockerenv --exclude=README.md --exclude=bin --exclude=boot --exclude=config.yml --exclude=dev --exclude=entrypoint.sh --exclude=etc --exclude=github --exclude=home --exclude=lib --exclude=lib64 --exclude=media --exclude=mnt --exclude=opt --exclude=proc --exclude=node_modules --exclude=package-lock.json --exclude=package.json --exclude=poetry.lock --exclude=pyproject.toml --exclude=run --exclude=root --exclude=sbin --exclude=src --exclude=srv --exclude=sys --exclude=tmp --exclude=usr --exclude=var ./ ./github/workspace
+cd ./github/workspace
+git add .
+git commit -m "Adding LDES fragments for branch $INPUT_BRANCH"
+git push origin main
 
+# Check if the LDES branch exists, if not, create it
+if git show-ref --quiet refs/heads/LDES; then
+    echo "LDES branch exists. Checking out LDES branch."
+    git checkout LDES
+else
+    echo "LDES branch does not exist. Creating and checking out LDES branch."
+    git checkout -b LDES
+fi
 
+cd ../..
+# copy over overything in the LDES folder to ./github/workspace
+rsync --recursive --progress -avzh --exclude=.git --exclude=.github --exclude=entrypoint.sh --exclude=Dockerfile --exclude=README.md --exclude=LICENSE --exclude=.gitignore --exclude=.dockerignore --exclude=.env --exclude=package.json --exclude=poetry.lock --exclude=pyproject.toml --exclude=requirements.txt --exclude=package-lock.json --exclude=actions.yml ./LDES/ ./github/workspace/
 
-
-# copy over everything in ./ back to ./github/workspace
-# overwrite everything in ./github/workspace
-# do not copy over the .git folder and the .github folder 
-# also do not copy over the following files:
-# - entrypoint.sh
-# - Dockerfile
-# - README.md
-# - LICENSE
-# - .gitignore
-# - .dockerignore
-# - .env
-# package.json
-# poetry.lock
-# pyproject.toml
-# requirements.txt
-# everything in src folder and recursively under src folder
-# package-lock.json
-# actions.yml
+cd ./github/workspace
+git add .
+git commit -m "Adding LDES fragments for branch $INPUT_BRANCH"
+git push origin LDES
