f4620674dca7fd6a15191084d69970124993321a
cedricdcc <cedricdecruw@gmail.com>
2025-09-02 14:24:15 +0200

    Fix symlink creation for latest.ttl in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -77,7 +77,7 @@ rsync --recursive --progress -avzhq \
 cd ./github/workspace
 
 # make a symlink from /LDES/latest.ttl to feed
-ln -sf /LDES/latest.ttl feed
+ln -sf /LDES/latest.ttl /LDES/feed
 git add .
 git commit -m "Adding LDES fragments for branch $INPUT_BRANCH"
 git push origin main
@@ -175,7 +175,7 @@ rsync --recursive --progress -avzhq \
 
 cd ./github/workspace
 # symlink feed to latest
-ln -sf /LDES/latest.ttl feed
+ln -sf /LDES/latest.ttl /LDES/feed
 git add .
 git commit -m "Adding LDES fragments made from processing data in branch $INPUT_BRANCH"
 git push origin gh-pages
